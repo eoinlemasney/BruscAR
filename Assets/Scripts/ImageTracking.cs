f3d3c3114dd0854b9e3d815fb9ee25d085f375e6
Eoin Le Masney <eoinlemasney@gmail.com>
2020-12-14 23:46:44 +0000

    Updates to AR Scence
    Stabilizing AR anchors
    Adding in sounds
    Adding in AR images

--- Assets/Scripts/ImageTracking.cs
+++ Assets/Scripts/ImageTracking.cs
@@ -16,23 +16,55 @@ public class ImageTracking : MonoBehaviour
     private Dictionary<string, GameObject> spawnedPrefabs = new Dictionary<string, GameObject>();
     private ARTrackedImageManager trackedImageManager;
 
-    public Text scoreLabel;
+    public Text itemInfoText;
+    public Text itemNameText;
+    public Text extraInfoText;
 
     public RawImage scanImageIcon;
 
-    private Vector3 scaleFactor = new Vector3(0.2f, 0.2f, 0.2f);
+    private Vector3 scaleFactor = new Vector3(0.1f, 0.1f, 0.1f);
+
+    private Vector3 fixedPosition;
+
+    public float speedRotation = 2f;
+    public float maxRotation = 30f;
+
+    public RaycastHit hit;
+    public Ray ray;
+
+    public AudioClip recyclingSound;
+    public AudioClip bingSound;
+    public AudioClip invalidBeep;
+
+    public AudioSource audioS;
+
+    //initiate floating text
+    public GameObject floatingTextPrefab;
+    public GameObject ParticleSystem;
+
+    public bool isTapped = false;
+
+    public string currentImage;
+
+    public string image_name;
 
     private void Awake()
     {
+        Debug.Log("Awake");
         trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
 
         foreach (GameObject prefab in placeablePrefabs)
         {
-            //zero so that it starts hidden, defaukt rortaion
-            GameObject newPrefab = Instantiate(prefab, Vector3.zero, Quaternion.identity);
-            //make sure name is correct for searching for it later
+            //zero so that it starts hidden, default rortation
+            Vector3 xyz = new Vector3(0f, 180f, 0f);
+            Quaternion correctRotation = Quaternion.Euler(xyz);
+            GameObject newPrefab = Instantiate(prefab, Vector3.zero, correctRotation);
+
+            //make sure name is correct for searching for it laters
             newPrefab.name = prefab.name;
             spawnedPrefabs.Add(prefab.name, newPrefab);
+            prefab.SetActive(false);
+    
         }
     }
 
@@ -58,60 +90,73 @@ public class ImageTracking : MonoBehaviour
         }
     }
 
-    //private void UpdateARImage(ARTrackedImage trackedImage)
-    //{
-    //    scanImageIcon.enabled = false;
-    //    string name = trackedImage.referenceImage.name;
-    //    Vector3 position = trackedImage.transform.position;
-
-    //    GameObject prefab = spawnedPrefabs[name];
-    //    prefab.SetActive(true);
-
-    //    foreach(GameObject go in spawnedPrefabs.Values)
-    //    {
-    //        if(go.name != name)
-    //        {
-    //            go.SetActive(false);
-    //        }
-    //    }
-    //    if (name == "Recycling")
-    //    {
-    //        scoreLabel.text = "Item can be recycled. Tap Recycle Icon for more details.";
-    //    }
-    //    else if (name == "General")
-    //    {
-    //        scoreLabel.text = "Item cannot be recycled. Tap General Waste Icon for more details.";
-    //    }
-    //    else if (name == "Compost")
-    //    {
-    //        scoreLabel.text = "Item is compostable. Tap Compostable Icon for more details.";
-    //    }
-
-
-    //}
 
     private void UpdateARImage(ARTrackedImage trackedImage)
     {
+        string raw_image_name = trackedImage.referenceImage.name;
         scanImageIcon.enabled = false;
-        string name = trackedImage.referenceImage.name;
-        Vector3 position = trackedImage.transform.position;
+        Vector3 position = trackedImage.transform.position + trackedImage.transform.up * 0.2f + trackedImage.transform.forward * 0.2f;
+        fixedPosition = position;
 
-        if (name == "Recycling")
-        {
-            scoreLabel.text = "Item can be recycled. Tap Recycle Icon for more details.";
-        }
-        else if (name == "General")
-        {
-            scoreLabel.text = "Item cannot be recycled. Tap General Waste Icon for more details.";
-        }
-        else if (name == "Compost")
-        {
-            scoreLabel.text = "Item is compostable. Tap Compostable Icon for more details.";
-        }
 
+        ReturnItemName(raw_image_name);
+        string name = DetectImageGroup(raw_image_name);
         AssignGameObject(name, position);
 
 
+        
+
+
+    }
+
+    public string DetectImageGroup(string raw_image_name)
+    {
+        if (raw_image_name.Contains("Recycling"))
+        {
+            return "Recycling";
+        }
+        else if (raw_image_name.Contains("General"))
+        {
+            return "General";
+        }
+
+        else if (raw_image_name.Contains("Compost"))
+        {
+            return "Compost";
+        } else
+        {
+            return raw_image_name;
+        }
+    }
+
+    void ReturnItemName(string raw_image_name)
+    {
+        if (raw_image_name.Contains("CokeCan"))
+        {
+            itemNameText.text = "Coke Can Detected.";
+        }
+        else if (raw_image_name.Contains("Innocent"))
+        {
+            itemNameText.text = "Innocent Bottle Detected.";
+
+        }
+
+        else if (raw_image_name.Contains("BarrysTea"))
+        {
+            itemNameText.text = "Barry's Tea Detected.";
+
+        }
+
+        else if (raw_image_name.Contains("TerrysChocolateGeneral"))
+        {
+            itemNameText.text = "Terry's Chocolate Orange Detected.";
+
+        }
+        else
+        {
+            itemNameText.text = "";
+        }
+        
     }
 
     void AssignGameObject(string name, Vector3 newPosition)
@@ -120,12 +165,18 @@ public class ImageTracking : MonoBehaviour
         {
             spawnedPrefabs[name].SetActive(true);
             spawnedPrefabs[name].transform.position = newPosition;
+            spawnedPrefabs[name].transform.rotation = Quaternion.Euler(0f, 180f, 0f);
             spawnedPrefabs[name].transform.localScale = scaleFactor;
             foreach (GameObject go in spawnedPrefabs.Values)
             {
                 if (go.name != name)
                 {
                     go.SetActive(false);
+                }
+                else
+                {
+                    UpdateItemText(name);
+
                 }
             }
 
@@ -135,4 +186,162 @@ public class ImageTracking : MonoBehaviour
 
 
     }
-}
+
+    void Update()
+    {
+        foreach (GameObject go in spawnedPrefabs.Values)
+        {
+            if (go.activeInHierarchy == true)
+            {
+                go.transform.position = fixedPosition;
+                if (isTapped == false)
+                {
+                    RotateObject(go, 2f);
+
+                    extraInfoText.text = "";
+                    UpdateItemText(go.transform.name);
+
+                }
+                else
+                {
+                    SwayObject(go);
+                    UpdateExtraInfo(go);
+                    itemInfoText.text = "";
+
+
+                }
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.name.Contains("Recycling"))
+                {
+
+                    PlayParticles(hit.collider.gameObject);
+
+                    if (isTapped == false)
+                    {
+                        audioS.PlayOneShot(recyclingSound);
+                        isTapped = true;
+
+                    } else
+                    {
+                        isTapped = false;
+                    }
+                }
+
+                else if (hit.collider.gameObject.name == "General")
+                {
+                    PlayParticles(hit.collider.gameObject);
+
+                    if (isTapped == false)
+                    {
+                        audioS.PlayOneShot(invalidBeep);
+                        isTapped = true;
+                    } else
+                    {
+                        isTapped = false;
+                    }
+                }
+
+                else if (hit.collider.gameObject.name == "Compost")
+                {
+                    PlayParticles(hit.collider.gameObject);
+
+                    if (isTapped == false)
+                    {
+                        audioS.PlayOneShot(bingSound);
+                        isTapped = true;
+                    }
+                    else
+                    {
+                        isTapped = false;
+                    }
+                }
+
+
+
+
+            }
+            }
+
+        }
+        void SwayObject(GameObject activeObject)
+        {
+
+            activeObject.transform.rotation = Quaternion.Euler(0f, 180f, maxRotation * Mathf.Sin(Time.time * speedRotation));
+
+        }
+
+        void RotateObject(GameObject activeObject, float rotateSpeed)
+        {
+
+            activeObject.transform.Rotate(0, rotateSpeed, 0);
+        }
+
+        void UpdateExtraInfo(GameObject activeObject)
+        {
+            string name = activeObject.name;
+
+            if (name == "Recycling")
+            {
+                extraInfoText.text = "Can should be Clean, Dry & Loose!";
+            }
+            else if (name == "General")
+            {
+                extraInfoText.text = "Unfortunately not all items are recycling. Check Tips&Tricks for more information.";
+            }
+            else if (name == "Compost")
+            {
+                extraInfoText.text = "Item is compostable.";
+            }
+        }
+
+        void UpdateItemText(string name)
+        {
+            if (name.Contains("Recycling"))
+            {
+                itemInfoText.text = "Item can be recycled. Tap Recycle Icon for more details.";
+
+            }
+            else if (name == "General")
+            {
+                itemInfoText.text = "Item cannot be recycled. Tap General Waste Icon for more details!";
+            }
+            else if (name == "Compost")
+            {
+                itemInfoText.text = "Item is compostable. Tap Compostable Icon for more details.";
+
+            if (itemNameText.text == "Barry's Tea Detected.")
+            {
+                itemInfoText.text = "Tea bags are compostable but the box must be recycled.";
+            }
+        }
+
+        }
+
+    void PlayParticles(GameObject activeObject)
+    {
+        if (!activeObject.transform.Find("Particle System").GetComponent<ParticleSystem>().isPlaying)
+        {
+
+            activeObject.transform.Find("Particle System").GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            activeObject.transform.Find("Particle System").GetComponent<ParticleSystem>().Stop();
+        }
+
+    }
+    }
+
+
+
+
---
+++ Assets/Scripts/ParticlesSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private GameObject ParticlesObject;
+
+    private float frequencey;
+
+    private float timeElapsed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
